2c3b3de00e36ddb91a376e678f731c1a3c6d9340
Sonny <sonnypollet@hotmail.fr>
2023-05-23 16:40:35 +0200

    fix: get materiaux as array

--- routers/displayMateriau.js
+++ routers/displayMateriau.js
@@ -5,7 +5,8 @@ const displayMatRouter = new Router();
 let materiau
 
 displayMatRouter.get("/displayMateriau/:id", async (req, res) => {
-  materiau = await Materiaux.find({ _id: req.params.id });
+  const materiaux = await Materiaux.find({ _id: req.params.id });
+  materiau = materiaux[0];
   res.render("displayMateriau", { materiau })
 });
 
